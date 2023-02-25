509566e83aeb37dd1e37e9fe8e73fad31f8ab2c0
unknown <kjh13710362237@outlook.com>
2023-02-25 11:11:27 +0800

    校园网\NA:更新了逻辑，新增了自动summary，输出改为中文

--- 校园网/Network_Alive.py
+++ 校园网/Network_Alive.py
@@ -5,13 +5,12 @@ from datetime import datetime
 from time import sleep, time
 
 
-debug = 0
-if not debug:
-    os.system("")
-
-
 def report_time() -> str:
-    return datetime.now().strftime("%H:%M:%S.%f")
+    return datetime.now().strftime("%H:%M:%S")
+
+
+def date_log() -> int:
+    return int(datetime.now().strftime("%d"))
 
 
 def log_op(operation: int):
@@ -32,41 +31,64 @@ def relogin(interval=2):
     sleep(interval)
 
 
+def summary():
+    os.system("cls")
+    rpd = date_log()
+    print(f"今日({rpd:02d}日)概况:")
+    print(
+        f"失败: {statistic['失败']:02d}, 成功: {statistic['成功']:02d}, 强制: {statistic['强制']:02d}, 跳过: {statistic['跳过']:02d}")
+    print(38*'-')
+    print("自动重登记录：")
+    for log in fail_log:
+        if log[0] == rpd:
+            print(f"  - {log[1]}")
+        else:
+            fail_log.remove(log)
+    print(fail_log)
+    return
 
+
+PING_TARGET = 'bilibili.com'
 if __name__ == "__main__":
+    statistic = {'失败': 0, '成功': 0, '强制': 0, '跳过': 0}
+    fail_log = []
     t1, t2 = 0, 0
     log_op(1)
     while 1:
         try:
             print(
-                f"[INFO][{report_time()}] Pinging...[Ctrl+C] to force re-login")
+                f"[INFO][{report_time()}] 正在ping {PING_TARGET}, [Ctrl+C] 强制重登")
 
             try:
                 t1 = time()
-                os.popen("ping bilibili.com -n 2").readlines()
+                os.popen(f"ping {PING_TARGET} -n 2").readlines()
                 t2 = time()
 
             except KeyboardInterrupt:
-                print(
-                    f"[WARN][{report_time()}] User: [Ctrl+C] Skip Pingand re-login")
+                statistic['强制'] += 1
+                print(f"[USER][{report_time()}] [Ctrl+C] 强制重登")
                 relogin()
                 continue
 
             if t2-t1 > 4.5:
-                print(f"[WARN][{report_time()}] Network status: Offline")
+                statistic['失败'] += 1
+                fail_log.append([date_log(), report_time()])
+                print(f"[WARN][{report_time()}] ping 判定：离线")
                 relogin()
 
             else:
-                print(f"[INFO][{report_time()}] Network status: Normal")
+                statistic['成功'] += 1
+                print(f"[INFO][{report_time()}] ping 判定：在线")
+                if (statistic["失败"]+statistic["成功"]+statistic["强制"])%5==0:
+                    summary()
                 print(
-                    f"[INFO][{report_time()}] Sleep for 600s, [Ctrl+C] to force re-login\n")
+                    f"[INFO][{report_time()}] 休眠十分钟， [Ctrl+C] 跳过休眠")
                 sleep(600)
-                print(f"[INFO][{report_time()}] Awake from normal sleep")
 
         except KeyboardInterrupt:
+            statistic['跳过'] += 1
             print(
-                f"[WARN][{report_time()}] User: [Ctrl+C] Skip sleep and re-login")
-            relogin()
+                f"[USER][{report_time()}] [Ctrl+C] 跳过休眠")
             continue
 
         except Exception as e:
