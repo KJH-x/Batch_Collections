a681d5ed7a41ef430a9f669d76d6e4da6ddb1a12
unknown <kjh13710362237@outlook.com>
2023-04-15 12:51:33 +0800

    安全性更新，改用新的函数方法

--- Click to run/GETBING.py
+++ Click to run/GETBING.py
@@ -2,95 +2,84 @@
 import sys
 import os
 from traceback import print_exc
-
 import yaml
 import re
-
 import requests
 import json
-import pyperclip
+import shutil
+import time
 
 
-def read_config():
-    """
-    加载yaml配置文件
-    """
-    yaml_path = ".\\config.yaml"
+def read_config(yaml_path):
     try:
-        # open config
         with open(yaml_path, "r", encoding="utf-8") as config_file:
-            data = yaml.load(config_file, Loader=yaml.FullLoader)
+            data = yaml.load(config_file, Loader=yaml.SafeLoader)
             return data
     except:
         input(print_exc())
         exit()
 
 
+def get_response(url, headers):
+    try:
+        response = requests.get(url=url, headers=headers)
+        response.raise_for_status()
+        return response
+    except requests.exceptions.RequestException as e:
+        raise Exception(f"Request failed: {str(e)}")
+
+
 def main():
     try:
-        os.system("chcp 65001|cls")
+        os.system("chcp 65001 >nul")
         os.chdir(sys.path[0])
-        data = read_config()
+        data = read_config(".\\config.yaml")
+
         UA = {'user-agent': data["net"]["user_agent"]}
         resolution = "1920x1080"
-        DL_temp_track = data["file"]["temp"]
-        static_track = data["file"]["storage"]
+        download_path = data["file"]["temp"]
+        static_path = data["file"]["storage"]
 
-        clipboard_temp = pyperclip.paste()
-        if not os.path.exists(DL_temp_track):
-            print("no such folder")
-            # 在当前路径创建img/文件夹
-            os.makedirs(DL_temp_track)
-            print("created target folder")
-        if not os.path.exists(static_track):
-            print("no such folder")
-            # 在当前路径创建img/文件夹
-            os.makedirs(static_track)
-            print("created target folder")
-
-        os.system("dir "+static_track+" /D /B /A:-D |clip")
-        existed_list = list(
-            pyperclip.paste()
-            .replace(".jpg", "").replace(".png", "").
-            splitlines()
+        if not os.path.exists(download_path):
+            print(f"不存在:{download_path}")
+            os.makedirs(download_path)
+            print(f"已创建:{download_path}")
+        if not os.path.exists(static_path):
+            print(f"不存在:{static_path}")
+            os.makedirs(static_path)
+            print(f"已创建:{static_path}")
+        get_file_list(static_path)
+        existed_list = get_file_list(static_path,False)
+        print(f"正在查询图片列表")
+        reponse_json = json.loads(
+            get_response(
+                url="https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=7&mkt=zh-CN",
+                headers=UA
+                ).text
         )
-        pyperclip.copy(clipboard_temp)
-
-        response = requests.get(
-            url="https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=7&mkt=zh-CN",
-            headers=UA
-        )
-        jsonData = json.loads(response.text)
 
         for serial in range(0, 7):
-            urlbase = (jsonData['images'][serial]['urlbase'])
-            copyright = str(jsonData['images'][serial]['copyright'])
+            pic_url_base = (reponse_json['images'][serial]['urlbase'])
+            pic_copyright = str(reponse_json['images'][serial]['copyright'])
+            pic_name = re.findall('^([^,，（()]+)', pic_copyright)[0]
+            
 
-            pic_name = copyright[0:copyright.find('，', 0)]
+            x1, x2 = map(lambda x: x.end() if x else len(pic_name),
+                        [re.match('.*（', pic_name), re.match('.*\(', pic_name)])
 
-            x1 = pic_name.find('（', 0) \
-                if pic_name.find('（', 0) != -1 \
-                else len(pic_name)
+            pic_name = pic_name[:min(x1, x2)]
 
-            x2 = pic_name.find('(', 0) \
-                if pic_name.find('(', 0) != -1 \
-                else len(pic_name)
-
-            pic_name = pic_name[0:min(x1, x2)]
             pic_name = re.sub("[ 　（）,，]", "", pic_name)
-
-            url = f"http://www.bing.com{urlbase}_{resolution}.jpg"
-            pictureTrack = DL_temp_track+"\\"+pic_name+".jpg"
-
+            url = f"http://www.bing.com{pic_url_base}_{resolution}.jpg"
+            pic_save_path = f"{download_path}\\{pic_name}.jpg"
             if pic_name in existed_list:
-                print(pic_name, " had already downloaded!")
+                print(f"已下载：{pic_name}")
             else:
-                print(f"Downloading {pic_name}")
-                saveImg(url, pictureTrack)
-
-        xcopy(fromTrack=DL_temp_track, toTrack=static_track)
+                print(f"正在下载：{pic_name}")
+                saveImg(url, pic_save_path)
+        xcopy(fromTrack=download_path, toTrack=static_path)
         print("All Done!")
-        input("Press Enter to exit")
+        time.sleep(3)
     except Exception:
         input(print_exc())
 
@@ -102,25 +91,45 @@ def saveImg(imgUrl, saveAs):
             picture.write(pic)
         return 1
     except Exception:
-        input(print_exc())
+        print("Failed to save image!")
+        print_exc()
         return 0
 
 
-def xcopy(fromTrack, toTrack):
+def get_file_list(folder_path: str, flag=True) -> list:
+    """
+    Get a list of file names in the folder_path.
 
-    os.system("dir "+str(fromTrack)+" /D /B /A:-D | clip")
-    file_list_fr = pyperclip.paste().split("\n")
-    os.system("dir "+str(toTrack)+" /D /B /A:-D | clip")
-    file_list_to = pyperclip.paste().split("\n")
+    Args:
+        folder_path (str): the folder path
+        flag (bool): if True, the file name will contain the extension, otherwise not
+
+    Returns:
+        list: a list of file names
+    """
+    if not os.path.exists(folder_path):
+        return []
+
+    file_list = os.listdir(folder_path)
+    if flag:
+        return file_list
+    else:
+        return [os.path.splitext(file_name)[0] for file_name in file_list]
+
+
+def xcopy(fromTrack, toTrack):
+    fromTrack = os.path.abspath(fromTrack)
+    toTrack = os.path.abspath(toTrack)
+    if not os.path.exists(fromTrack):
+        os.makedirs(fromTrack)
+    if not os.path.exists(toTrack):
+        os.makedirs(toTrack)
+    file_list_fr = get_file_list(fromTrack)
+    file_list_to = get_file_list(toTrack)
     copy_list = [x for x in file_list_fr if x not in file_list_to]
     for file_name in copy_list:
-        if not os.path.exists(toTrack+"\\"+file_name):
-            command = "xcopy \""+fromTrack+"\\" +\
-                str(file_name)+"\" \""+str(toTrack)+"\" /D"
-            os.system(command)
-        else:
-            pass
-    return 1
+        if not os.path.exists(os.path.join(toTrack, file_name)):
+            shutil.copy(os.path.join(fromTrack, file_name), toTrack)
 
 
 if __name__ == '__main__':
--- Clipboard Utilities/网址中文转换器Url2HEX.py
+++ Clipboard Utilities/网址中文转换器Url2HEX.py
@@ -7,14 +7,5 @@ Author :KJH-x
 from urllib.parse import quote
 import string
 import pyperclip
-# Vlist = []
-# contentDir = ""
-# downloadlink = []
-# clipboard = ""
-# for v in Vlist:
-# url = quote(contentDir+v, safe=string.printable)
-# downloadlink.append(url)
-# clipboard += f"<a href=\"{url}\" target=\"_blank\">{v}</a><br>"
-# print(downloadlink)
 
-pyperclip.copy(quote(pyperclip.paste(), safe=string.printable))
+pyperclip.copy(quote(pyperclip.paste().strip().replace(' ','%20'), safe=string.printable))
