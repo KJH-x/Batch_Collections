9ce72c578daaecca8c3e0aa92efc2222d083883c
NappingSodion <kjh13710362237@outlook.com>
2024-02-29 00:58:06 +0800

    +-去除了多于信息

--- Interactive/合并alpha PNG.py
+++ Interactive/合并alpha PNG.py
@@ -18,17 +18,9 @@ def merge_images(color_image_path, alpha_image_path, output_image_path):
     # 保存结果
     color_img.save(output_image_path)
 
-# 示例用法
-# for text in ["battle_finish_hub#0","bossrush_relic#0","bossrush_stage#0"]:
-#     color_image_path = rf'P:\BossRush_uiPack_common\[uc]bossrush\{text}s.png'
-#     alpha_image_path = rf'P:\BossRush_uiPack_common\[uc]bossrush\{text}a.png'
-#     output_image_path = rf'P:\BossRush_uiPack_common\[uc]bossrush\{text}.png'
-#     # bossrush_milestone#0.png bossrush_milestone#1
-#     merge_images(color_image_path, alpha_image_path, output_image_path)
-# p:\BossRush_uiPack_common\bossrush\battle\act1bossrush_battle_ui_plugin\bossrush_atlas#0a.png
-    
-color_image_path = rf'p:\new_attach\Unpacked_1708870773\Android\activity\[uc]act4bossrush\squad_plugin_atlas#0s.png'
-alpha_image_path = rf'p:\new_attach\Unpacked_1708870773\Android\activity\[uc]act4bossrush\squad_plugin_atlas#0a.png'
-output_image_path = rf'p:\new_attach\Unpacked_1708870773\Android\activity\[uc]act4bossrush\squad_plugin_atlas#0.png'
+
+color_image_path = rf'...'
+alpha_image_path = rf'...'
+output_image_path = rf'...'
 # bossrush_milestone#0.png bossrush_milestone#1
 merge_images(color_image_path, alpha_image_path, output_image_path)
